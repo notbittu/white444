7834f986e2d1f54610f767bce1bf7942b1f2262a
notbittu <ankitsen.in@gmail.com>
2025-03-13 10:19:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def color_suggestion():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
